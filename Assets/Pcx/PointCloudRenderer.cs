3ede669ec622671aaf102cd81c21cba4d6ae7914
Keijiro Takahashi <keijiro@gmail.com>
2017-10-28 01:28:45 +0900

    2nd implementation of compute shader

--- Assets/Pcx/PointCloudRenderer.cs
+++ Assets/Pcx/PointCloudRenderer.cs
@@ -15,22 +15,26 @@ namespace Pcx
 
         #region Internal resources
 
-        [SerializeField, HideInInspector] Shader _pointShader;
-        [SerializeField, HideInInspector] Shader _geometryShader;
-        [SerializeField, HideInInspector] Shader _discShader;
-        [SerializeField, HideInInspector] ComputeShader _converter;
+        [SerializeField, HideInInspector] Shader _shader;
+        [SerializeField, HideInInspector] ComputeShader _sorter;
 
         #endregion
 
         #region Private variables
 
-        Material _pointMaterial;
-        Material _geometryMaterial;
-        Material _discMaterial;
-
         ComputeBuffer _pointBuffer;
-        ComputeBuffer _triangleBuffer;
-        ComputeBuffer _drawArgsBuffer;
+
+        Material _material1;
+        Material _material2;
+        Material _material3;
+
+        ComputeBuffer _drawBuffer1;
+        ComputeBuffer _drawBuffer2;
+        ComputeBuffer _drawBuffer3;
+
+        ComputeBuffer _argsBuffer1;
+        ComputeBuffer _argsBuffer2;
+        ComputeBuffer _argsBuffer3;
 
         #endregion
 
@@ -47,30 +51,42 @@ namespace Pcx
             if (_pointBuffer != null)
             {
                 _pointBuffer.Release();
-                _triangleBuffer.Release();
-                _drawArgsBuffer.Release();
+
+                _drawBuffer1.Release();
+                _drawBuffer2.Release();
+                _drawBuffer3.Release();
+
+                _argsBuffer1.Release();
+                _argsBuffer2.Release();
+                _argsBuffer3.Release();
 
                 _pointBuffer = null;
-                _triangleBuffer = null;
-                _drawArgsBuffer = null;
+
+                _drawBuffer1 = null;
+                _drawBuffer2 = null;
+                _drawBuffer3 = null;
+
+                _argsBuffer1 = null;
+                _argsBuffer2 = null;
+                _argsBuffer3 = null;
             }
         }
 
         void OnDestroy()
         {
-            if (_pointMaterial != null)
+            if (_material1 != null)
             {
                 if (Application.isPlaying)
                 {
-                    Destroy(_pointMaterial);
-                    Destroy(_geometryMaterial);
-                    Destroy(_discMaterial);
+                    Destroy(_material1);
+                    Destroy(_material2);
+                    Destroy(_material3);
                 }
                 else
                 {
-                    DestroyImmediate(_pointMaterial);
-                    DestroyImmediate(_geometryMaterial);
-                    DestroyImmediate(_discMaterial);
+                    DestroyImmediate(_material1);
+                    DestroyImmediate(_material2);
+                    DestroyImmediate(_material3);
                 }
             }
         }
@@ -78,81 +94,81 @@ namespace Pcx
         void Update()
         {
             // Lazy initialization
-            if (_pointMaterial == null)
+            if (_material1 == null)
             {
-                _pointMaterial = new Material(_pointShader);
-                _pointMaterial.hideFlags = HideFlags.DontSave;
-                _pointMaterial.EnableKeyword("_COMPUTE_BUFFER");
+                _material1 = new Material(_shader);
+                _material2 = new Material(_shader);
+                _material3 = new Material(_shader);
 
-                _geometryMaterial = new Material(_geometryShader);
-                _geometryMaterial.hideFlags = HideFlags.DontSave;
-                _geometryMaterial.EnableKeyword("_COMPUTE_BUFFER");
-
-                _discMaterial = new Material(_discShader);
-                _discMaterial.hideFlags = HideFlags.DontSave;
+                _material1.hideFlags = HideFlags.DontSave;
+                _material2.hideFlags = HideFlags.DontSave;
+                _material3.hideFlags = HideFlags.DontSave;
             }
 
             if (_pointBuffer == null && _source != null)
             {
                 _pointBuffer = _source.CreateComputeBuffer();
 
-                _triangleBuffer = new ComputeBuffer(
-                    _source.pointCount * 3 * 20, sizeof(float) * 4 * 4,
-                    ComputeBufferType.Append
-                );
+                _drawBuffer1 = new ComputeBuffer(_source.pointCount, 4 * 4, ComputeBufferType.Append);
+                _drawBuffer2 = new ComputeBuffer(_source.pointCount, 4 * 4, ComputeBufferType.Append);
+                _drawBuffer3 = new ComputeBuffer(_source.pointCount, 4 * 4, ComputeBufferType.Append);
 
-                _drawArgsBuffer = new ComputeBuffer(
-                    1, 4 * sizeof(uint),
-                    ComputeBufferType.IndirectArguments
-                );
-                _drawArgsBuffer.SetData(new uint[] { 3, 0, 0, 0 });
+                _argsBuffer1 = new ComputeBuffer(1, 4 * 4, ComputeBufferType.IndirectArguments);
+                _argsBuffer2 = new ComputeBuffer(1, 4 * 4, ComputeBufferType.IndirectArguments);
+                _argsBuffer3 = new ComputeBuffer(1, 4 * 4, ComputeBufferType.IndirectArguments);
+
+                _argsBuffer1.SetData(new uint[] { 0, 1, 0, 0 });
+                _argsBuffer2.SetData(new uint[] { 3 * 4, 0, 0, 0 });
+                _argsBuffer3.SetData(new uint[] { 3 * 12, 0, 0, 0 });
             }
         }
 
         void OnRenderObject()
         {
-            if (_pointMaterial == null || _pointBuffer == null) return;
+            if (_material1 == null || _pointBuffer == null) return;
 
-            if (_pointSize == 0)
-            {
-                _pointMaterial.SetPass(0);
-                _pointMaterial.SetColor("_Color", _pointColor);
-                _pointMaterial.SetMatrix("_Transform", transform.localToWorldMatrix);
-                _pointMaterial.SetBuffer("_PointBuffer", _pointBuffer);
-                Graphics.DrawProcedural(MeshTopology.Points, _source.pointCount, 1);
-            }
-            else
-            {
-#if true
-                _geometryMaterial.SetPass(0);
-                _geometryMaterial.SetColor("_Color", _pointColor);
-                _geometryMaterial.SetMatrix("_Transform", transform.localToWorldMatrix);
-                _geometryMaterial.SetBuffer("_PointBuffer", _pointBuffer);
-                _geometryMaterial.SetFloat("_PointSize", _pointSize);
-                Graphics.DrawProcedural(MeshTopology.Points, _source.pointCount, 1);
-#else
-                var proj = GL.GetGPUProjectionMatrix(Camera.current.projectionMatrix, true);
-                var view = Camera.current.worldToCameraMatrix;
+            var proj = GL.GetGPUProjectionMatrix(Camera.current.projectionMatrix, true);
+            var view = Camera.current.worldToCameraMatrix;
 
-                _converter.SetFloat("ScreenHeight", Camera.current.pixelHeight);
-                _converter.SetVector("Tint", _pointColor);
-                _converter.SetVector("Extent", new Vector2(Mathf.Abs(proj[0, 0]), Mathf.Abs(proj[1, 1])) * _pointSize);
-                _converter.SetMatrix("Transform", proj * view * transform.localToWorldMatrix);
+            _sorter.SetFloat("ScreenHeight", Camera.current.pixelHeight);
+            _sorter.SetVector("Extent", new Vector2(Mathf.Abs(proj[0, 0]), Mathf.Abs(proj[1, 1])) * _pointSize);
+            _sorter.SetMatrix("Transform", proj * view * transform.localToWorldMatrix);
 
-                var kernel = _converter.FindKernel("Main");
-                _converter.SetBuffer(kernel, "PointBuffer", _pointBuffer);
-                _converter.SetBuffer(kernel, "TriangleBuffer", _triangleBuffer);
+            var kernel = _sorter.FindKernel("Main");
+            _sorter.SetBuffer(kernel, "PointBuffer", _pointBuffer);
+            _sorter.SetBuffer(kernel, "DrawBuffer1", _drawBuffer1);
+            _sorter.SetBuffer(kernel, "DrawBuffer2", _drawBuffer2);
+            _sorter.SetBuffer(kernel, "DrawBuffer3", _drawBuffer3);
 
-                _triangleBuffer.SetCounterValue(0);
-                _converter.Dispatch(kernel, _source.pointCount / 128, 1, 1);
+            _drawBuffer1.SetCounterValue(0);
+            _drawBuffer2.SetCounterValue(0);
+            _drawBuffer3.SetCounterValue(0);
 
-                ComputeBuffer.CopyCount(_triangleBuffer, _drawArgsBuffer, 4);
+            _sorter.Dispatch(kernel, _source.pointCount / 128, 1, 1);
 
-                _discMaterial.SetPass(0);
-                _discMaterial.SetBuffer("_TriangleBuffer", _triangleBuffer);
-                Graphics.DrawProceduralIndirect(MeshTopology.Triangles, _drawArgsBuffer, 0);
-#endif
-            }
+            ComputeBuffer.CopyCount(_drawBuffer1, _argsBuffer1, 0);
+            ComputeBuffer.CopyCount(_drawBuffer2, _argsBuffer2, 4);
+            ComputeBuffer.CopyCount(_drawBuffer3, _argsBuffer3, 4);
+
+            _material1.SetPass(0);
+            _material1.SetColor("_Tint", _pointColor);
+            _material1.SetMatrix("_Transform", transform.localToWorldMatrix);
+            _material1.SetBuffer("_PointBuffer", _drawBuffer1);
+            Graphics.DrawProceduralIndirect(MeshTopology.Points, _argsBuffer1, 0);
+
+            _material2.SetPass(1);
+            _material2.SetColor("_Tint", _pointColor);
+            _material2.SetFloat("_PointSize", _pointSize);
+            _material2.SetMatrix("_Transform", transform.localToWorldMatrix);
+            _material2.SetBuffer("_PointBuffer", _drawBuffer2);
+            Graphics.DrawProceduralIndirect(MeshTopology.Triangles, _argsBuffer2, 0);
+
+            _material3.SetPass(2);
+            _material3.SetColor("_Tint", _pointColor);
+            _material3.SetFloat("_PointSize", _pointSize);
+            _material3.SetMatrix("_Transform", transform.localToWorldMatrix);
+            _material3.SetBuffer("_PointBuffer", _drawBuffer3);
+            Graphics.DrawProceduralIndirect(MeshTopology.Triangles, _argsBuffer3, 0);
         }
 
         #endregion
---
+++ Assets/Pcx/Shaders/MultiMode.shader
@@ -0,0 +1,141 @@
+﻿Shader "Hidden/PCX/Multi Mode"
+{
+    Properties
+    {
+        [HDR] _Color("Tint", Color) = (1, 1, 1, 1)
+        _PointSize("Point Size", Float) = 0.05
+    }
+
+    CGINCLUDE
+
+    #include "Common.cginc"
+
+    struct Attributes
+    {
+        float4 position : POSITION;
+        half4 color : COLOR;
+    };
+
+    struct Varyings
+    {
+        float4 position : SV_POSITION;
+        half4 color : COLOR;
+        UNITY_FOG_COORDS(1)
+    };
+
+    half4 _Color;
+    float4x4 _Transform;
+    half _PointSize;
+
+    StructuredBuffer<float4> _PointBuffer;
+
+    half4 Fragment(Varyings input) : SV_Target
+    {
+        half4 c = input.color;
+        UNITY_APPLY_FOG(input.fogCoord, c);
+        return c;
+    }
+
+    float4 Disc(float4 pt, uint vid, uint div)
+    {
+        float4 pos = mul(_Transform, float4(pt.xyz, 1));
+        float4 origin = UnityObjectToClipPos(pos);
+        float2 extent = abs(UNITY_MATRIX_P._11_22 * _PointSize);
+
+        // Determine the number of slices based on the radius of the
+        // point on the screen.
+        float radius = extent.y / origin.w * _ScreenParams.y;
+        uint slices = min((radius + 1) / 5, 4) + 2;
+
+        uint i = vid % 3;
+        uint fin = vid / 3;
+
+        if (i == 0)
+        {
+            return origin;
+        }
+        else
+        {
+            float cs, sn;
+            sincos(UNITY_PI * 2 * (fin + i - 1) / div, sn, cs);
+            return origin + float4(extent * float2(sn, cs), 0, 0);
+        }
+    }
+
+    ENDCG
+
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+
+        Cull Off
+
+        Pass
+        {
+            CGPROGRAM
+
+            #pragma vertex Vertex
+            #pragma fragment Fragment
+            #pragma multi_compile_fog
+
+            Varyings Vertex(uint vid : SV_VertexID)
+            {
+                float4 pt = _PointBuffer[vid];
+                float4 pos = mul(_Transform, float4(pt.xyz, 1));
+                half4 col = UnpackColor32(asuint(pt.w));
+
+                Varyings o;
+                o.position = UnityObjectToClipPos(pos);
+                o.color = col * _Color;
+                UNITY_TRANSFER_FOG(o, o.position);
+                return o;
+            }
+
+            ENDCG
+        }
+
+        Pass
+        {
+            CGPROGRAM
+
+            #pragma vertex Vertex
+            #pragma fragment Fragment
+            #pragma multi_compile_fog
+
+            Varyings Vertex(uint vid : SV_VertexID, uint iid : SV_InstanceID)
+            {
+                float4 pt = _PointBuffer[iid];
+
+                Varyings o;
+                o.position = Disc(pt, vid, 4);
+                o.color = UnpackColor32(asuint(pt.w)) * _Color;
+                UNITY_TRANSFER_FOG(o, o.position);
+                return o;
+            }
+
+            ENDCG
+        }
+
+        Pass
+        {
+            CGPROGRAM
+
+            #pragma vertex Vertex
+            #pragma fragment Fragment
+            #pragma multi_compile_fog
+
+            Varyings Vertex(uint vid : SV_VertexID, uint iid : SV_InstanceID)
+            {
+                float4 pt = _PointBuffer[iid];
+
+                Varyings o;
+                o.position = Disc(pt, vid, 12);
+                o.color = UnpackColor32(asuint(pt.w)) * _Color;
+                UNITY_TRANSFER_FOG(o, o.position);
+                return o;
+            }
+
+            ENDCG
+        }
+    }
+}
